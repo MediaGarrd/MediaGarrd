0f9dcca50f5376330129ef5ced34ad4c4d4a97d7
Jackson Swindell <jackson1swindell@gmail.com>
2026-09-08 22:49:14 -0500

    Migrated codebases to their own repos

--- MediaGarrd-Client/src/main/java/wellatleastitried/mediaguardclient/dto/ConnectionStatusDto.java
+++
@@ -1,7 +0,0 @@
-package wellatleastitried.mediagarrdclient.dto;
-
-public record ConnectionStatusDto(String activeServer, boolean connected, ServerStatusDto serverStatus) {
-    public ConnectionStatusDto(String activeServer, boolean connected) {
-        this(activeServer, connected, null);
-    }
-}
